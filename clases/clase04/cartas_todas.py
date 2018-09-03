f30630c261778aa5ef9ec6183b53b754e9ff3b43
Diego Caro <diegocaro@gmail.com>
2018-09-03 14:21:12 -0300

    update clase04

--- clases/clase04/cartas_todas.py
+++ clases/clase04/cartas_todas.py
@@ -1,4 +1,3 @@
-from random import randrange
 TRAJES = ['Picas', 'Diamantes', 'Treboles', 'Corazones']
 VALORES = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
          'Jota', 'Reina', 'Rey', 'As']
