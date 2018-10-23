c4fff7be3300d08fcb201004456db72fbb8fa2b7
Diego Caro <diegocaro@gmail.com>
2018-10-23 12:39:21 -0300

    clase10

--- clases/clase08/performance.py
+++ clases/clase08/performance.py
@@ -12,15 +12,15 @@ def testfrozenset():
 
 if __name__ == '__main__':
     import timeit
-    trials = 100
+    trials = 20
     
     for f in ['testlist','testtuple','testset','testfrozenset']:
-        for m in [100000, 10**6, 10**7]:
+        for m in [100000, 10**6, 10**7, 10**8]:
             x = m - 1
             L = list(range(m))
             T = tuple(L)
             S = set(L)
             F = frozenset(L)
             
-            avg_time = timeit.timeit("{}()".format(f), number=trials, globals=globals())
+            avg_time = timeit.timeit("{}()".format(f), number=trials, globals=globals())/trials
             print('Checking {} with {} elements: {:.3f}s'.format(f,m, avg_time))
