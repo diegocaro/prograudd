f20077983e61e4bd9eacd96f9ac904e0e892e41b
Diego Caro <diegocaro@gmail.com>
2019-05-31 12:47:57 -0400

    update

--- clases/clase16/bouncingball-deluxe.py
+++ clases/clase16/bouncingball-deluxe.py
@@ -1,5 +1,5 @@
 import stddraw
-from ball import Ball
+from balldeluxe import Ball
 
 stddraw.setCanvasSize(500, 500)
 
@@ -7,9 +7,9 @@ stddraw.setXscale(-1.0, 1.0)
 stddraw.setYscale(-1.0, 1.0)
 
 balls = [
-    Ball(.480, .860, .015, .023, .05, stddraw.BLACK),
-    Ball(.480, .860, .030, .046, .05, stddraw.BLUE),
-    Ball(.180, .260, .040, .026, .05, stddraw.GREEN)
+    Ball(.480, .860, .015, .023, .05, stddraw.RED),
+    Ball(.480, .860, .030, .046, .1, stddraw.BLUE),
+    Ball(.180, .260, .040, .026, .2, stddraw.GREEN)
 ]
 
 while True:
